bde553aacabba74963aba3db51e30be7e055f7c2
Hong Jiang <j@1byte.io>
2019-04-29 20:00:19 +0800

    feat: add twitter card metadata

--- src/templates/article.jsx
+++ src/templates/article.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
 
 import Layout from '../components/layout';
 
@@ -7,6 +8,23 @@ export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <Layout>
+      <Helmet
+        title={data.site.siteMetadata.title + ': ' + post.frontmatter.title}
+        meta={[
+          {
+            name: 'twitter:card',
+            content: 'summary'
+          },
+          {
+            name: 'og:title',
+            content: post.frontmatter.title
+          },
+          {
+            name: 'og:description',
+            content: data.site.siteMetadata.title
+          }
+        ]}
+      />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -17,6 +35,11 @@ export default ({ data }) => {
 
 export const query = graphql`
   query BlogPostQuery($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
