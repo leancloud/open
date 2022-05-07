45af0de60524535686b33465b797e77773a6d01c
Hong Jiang <j@1byte.io>
2022-05-07 15:07:31 +0800

    add a paragraph about TDS.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,16 @@ export default ({ data }) => (
   <Layout>
     <div>
       <p>
+        <strong>
+          LeanCloud 已经于 2021 年并入 <a href="https://taptap.com">TapTap</a>
+          。本站的内容仅作为历史存档供参考。在进入 TapTap 后，LeanCloud
+          的原团队加入了开发者服务部门（TDS）。我们仍然在新的公司继续执行一些这里提到的继续适用的制度和规则，而
+          TapTap 也同样提倡开放、平等、透明的文化。如果你对 TDS
+          感兴趣，欢迎访问我们的<a href="https://blog.taptap.dev">博客</a>
+          ，我们也会继续开放可能对其他人有用的资源。
+        </strong>
+      </p>
+      <p>
         本站包含 <a href="https://leancloud.cn/">LeanCloud</a>{' '}
         开放的一些产品之外的资源，更多内容在逐步增加中。我们相信开放和透明会使一家公司更具长期竞争力，所以我们尽可能把信息公开。让所有人了解我们、监督我们的同时，也希望这些信息可以为他人参考和借鉴，发挥最大的价值。
       </p>
